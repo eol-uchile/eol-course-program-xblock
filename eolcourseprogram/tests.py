b43b4043c91aa52cbbfe8559bdecc29dfe834d03
matsalinas <matsalinas@uchile.cl>
2020-12-09 18:37:30 +0000

    XBlock tests

--- eolcourseprogram/tests.py
+++ eolcourseprogram/tests.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 
-
+import json
 from mock import patch, Mock
-
 
 from django.test import TestCase, Client
 
@@ -20,8 +19,21 @@ logger = logging.getLogger(__name__)
 
 XBLOCK_RUNTIME_USER_ID = 99
 
+class TestRequest(object):
+    # pylint: disable=too-few-public-methods
+    """
+    Module helper for @json_handler
+    """
+    method = None
+    body = None
+    success = None
+    params = None
+    headers = None
 
 class TestEolCourseProgramXBlock(UrlResetMixin, ModuleStoreTestCase):
+    """
+        XBlock tests
+    """
 
     def make_an_xblock(self, **kw):
         """
@@ -40,6 +52,7 @@ class TestEolCourseProgramXBlock(UrlResetMixin, ModuleStoreTestCase):
         field_data = DictFieldData(kw)
         xblock = EolCourseProgramXBlock(runtime, field_data, scope_ids)
         xblock.xmodule_runtime = runtime
+        xblock.course_id = course.id
         return xblock
 
     def setUp(self):
@@ -99,3 +112,62 @@ class TestEolCourseProgramXBlock(UrlResetMixin, ModuleStoreTestCase):
         test_result = self.xblock.workbench_scenarios()
         self.assertEqual(result_title, test_result[0][0])
         self.assertIn(basic_scenario, test_result[0][1])
+
+    def test_validate_default_field_data(self):
+        """
+            Validate that xblock is created successfully
+        """
+        self.assertEqual(self.xblock.display_name, 'Programa de Cursos')
+        self.assertEqual(self.xblock.program_id, None)
+
+    def test_student_view_without_configuration(self):
+        """
+            Check if error message is triggered when is not configured
+        """
+        student_view = self.xblock.student_view()
+        student_view_html = student_view.content
+        self.assertIn('El componente aún no se ha configurado.', student_view_html)
+        self.assertNotIn('class="eolcourseprogram_block"', student_view_html)
+
+    def test_student_view_with_configuration(self):
+        """
+            Check if error message is not triggered when is successfully configured
+        """
+        self.xblock.program_id = 5
+        student_view = self.xblock.student_view()
+        student_view_html = student_view.content
+        self.assertNotIn('El componente aún no se ha configurado.', student_view_html)
+        self.assertIn('class="eolcourseprogram_block"', student_view_html)
+
+    def test_author_view(self):
+        """
+            Test author view:
+            1. Without Configurations
+            2. With Configurations
+        """
+        author_view = self.xblock.author_view()
+        author_view_html = author_view.content
+        self.assertIn('<p>El componente aún <strong>no</strong> se ha configurado.</p>', author_view_html)
+        self.assertNotIn('<p>El componente está <strong>correctamente configurado</strong>. Ver en LMS.</p>', author_view_html)
+
+        self.xblock.program_id = 5
+        author_view = self.xblock.author_view()
+        author_view_html = author_view.content
+        self.assertNotIn('<p>El componente aún <strong>no</strong> se ha configurado.</p>', author_view_html)
+        self.assertIn('<p>El componente está <strong>correctamente configurado</strong>. Ver en LMS.</p>', author_view_html)
+
+    def test_studio_submit(self):
+        """
+            Test Studio submit handler
+        """
+        request = TestRequest()
+        request.method = 'POST'
+        post_data = {
+            'program_id': 999,
+        }
+        data = json.dumps(post_data)
+        request.body = data
+        request.params = post_data
+        response = self.xblock.studio_submit(request)
+        self.assertEqual(self.xblock.display_name, 'Programa de Cursos')
+        self.assertEqual(self.xblock.program_id, 999)
