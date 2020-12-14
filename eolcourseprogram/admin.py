535cc5d18989b5698e08f344cf6fada73f462375
matsalinas <matsalinas@uchile.cl>
2020-12-14 17:24:30 +0000

    Autocomplete fields course id (django admin)

--- eolcourseprogram/admin.py
+++ eolcourseprogram/admin.py
@@ -6,6 +6,7 @@ from django.contrib import admin
 from .models import EolCourseProgram
 
 class EolCourseProgramSetupAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['courses', 'final_course']
     list_display = ('program_name', 'is_active')
 
 admin.site.register(EolCourseProgram, EolCourseProgramSetupAdmin)
